6dc5cbb953700aa8bcb80d8a1024d5868ad51134
Rafa Martins <rafael.sm008@gmail.com>
2025-10-03 17:43:20 -0300

    Configuracoes de dependencias

---
+++ apigerenciamento/src/main/java/io/github/Projeto_Gerenciamento_de_Financas/apigerenciamento/ApiGerenciamentodeFinancasApplication.java
@@ -0,0 +1,13 @@
+package io.github.Projeto_Gerenciamento_de_Financas.apigerenciamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGerenciamentodeFinancasApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(ApiGerenciamentodeFinancasApplication.class, args);
+	}
+
+}
---
+++ apigerenciamento/src/test/java/io/github/Projeto_Gerenciamento_de_Financas/apigerenciamento/ApiGerenciamentodeFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.Projeto_Gerenciamento_de_Financas.apigerenciamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGerenciamentodeFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
